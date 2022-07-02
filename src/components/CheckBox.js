584ca7bbed12ccbc76a120ff995f85a015876994
shurik302 <balabuha1988@gmail.com>
2022-07-02 16:00:50 +0300

    Footer + button рассылки

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,53 +1,18 @@
-export function check(){
+export function check() {
     const checkB = document.querySelector('.BtnF')
-    /* console.log(checkB) */
     const checkBut = document.querySelector('.InputCheck')
-    /* console.log(checkBut) */
-    
-    /* checkB.addEventListener('click',()=>{ */
-        if (checkBut.checked) {
-            /* console.log('не нажат') */
-            checkB.type = 'submit'
-            console.log('Нажмите галочку')
-            checkB.classList.add('test')
-        }
-        else{
-            /* console.log('нажат') */
-            /* console.log(checkBut.checked) */
-            checkB.type = 'button'
-            checkB.classList.remove('test')
-            console.log('Галочка стоит')
-        }
-    /* }) */
-    
-
+    if (checkBut.checked) {
+        checkB.type = 'submit'
+        checkB.classList.add('test')
+    }
+    else {
+        checkB.type = 'button'
+        checkB.classList.remove('test')
+    }
 }
 export function checkBtn() {
     const checkB = document.querySelector('.BtnF')
     if (checkB.type == 'button') {
-        
         alert('Ознакомтесь с нашими правилами!')
     }
-    console.log('alert')
-
 }
-
-
-
-
-
-
-
-
-/* if (checkBut.checked == false) {
-    alert('Нажмите галочку')
-
-}
-else{
-    checkB.addEventListener('click',()=>{
-    console.log('нажат')
-    console.log(checkBut.checked)
-    alert('Галочка стоит')
-    
-})
-} */
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,12 @@ import '../stylesheets/Components.css';
 import './CheckBox'
 import { check } from "../components/CheckBox";
 import { checkBtn } from "../components/CheckBox";
+import facebook from '../images/facebook.png';
+import viber from '../images/viber.png';
+import whatsapp from '../images/whatsapp.png';
+import telegram from '../images/telegram.png';
+
+
 
 function Footer() {
     return (
@@ -55,7 +61,7 @@ function Footer() {
                             <i class="fa-solid fa-clock Con"></i>
                             <span className='ClockSpa'>Пн-Пт <br /> 7:00 - 16:00 (МСК)</span>
                         </li>
-                        <li><i class="fa-solid fa-location-dot Con"></i><a target="_blank" href='https://www.google.com/maps/place/Victorinox+Poland+Sp.+z+o.o./@52.2398727,20.9982136,17z/data=!3m1!4b1!4m5!3m4!1s0x471ecc795d83b53b:0x7f863911973584ac!8m2!3d52.2398439!4d21.0004207'> Victorinox Poland Salon Firmowy<br/>ul. Ptasia 6</a></li>
+                        <li><i class="fa-solid fa-location-dot Con"></i><a target="_blank" href='https://www.google.com/maps/place/Victorinox+Poland+Sp.+z+o.o./@52.2398727,20.9982136,17z/data=!3m1!4b1!4m5!3m4!1s0x471ecc795d83b53b:0x7f863911973584ac!8m2!3d52.2398439!4d21.0004207'> Victorinox Poland Salon Firmowy<br />ul. Ptasia 6</a></li>
                         <li><i class="fa-solid fa-envelope Con"></i>Условия оплаты <br /> и доставки</li>
                     </ol>
                     <ol>
@@ -79,51 +85,32 @@ function Footer() {
                             <form className='Input'>
                                 <input type='text' placeholder='example@gmail.com' />
                                 <button className='BtnF' type='button' onClick={checkBtn}>
-                                {/* <i class="fa-solid fa-chevron-right"></i> */}
+                                    {/* <i class="fa-solid fa-chevron-right"></i> */}
 
                                 </button>
                             </form>
                         </li>
                         <li className='CheckBOX'>
                             <input className='InputCheck' onClick={check} type='checkbox' /><span>Я прочитал Условия соглашения и <br />согласен с условиями </span>
-                            <button id="submit_2" >Проверить нажат ли checkbox</button>
                         </li>
                     </ol>
                 </div>
             </div>
+            <div className='MedaiSoc'>
+                <img src={facebook}/>
+                <img src={viber}/>
+                <img src={telegram}/>
+                <img src={whatsapp}/>
+                
+                
+            </div>
             <hr />
             <div className='FDetails'>
                 <p>Все материалы, размещенные на сайте, носят справочный характер и не являются публичной офертой, определяемойположениями Статьи 437 . При копировании материалов гиперссылка на www.zlatmax.com обязательна!</p>
-            <p>Златоустовские ножи www.zlatmax.com ©</p>
+                <p>Златоустовские ножи www.zlatmax.com ©</p>
             </div>
         </div>
     )
 }
 
 export default Footer
-
-
-
-
-
-/* if(test == false){
-    console.log('не выбран')
-}
-else{
-    console.log('выбран')
-}
- */
-
-/* (".CheckBOX").on("click", function () {
-    let checkboxStatus = $(this).prop("checked");
-
-    console.log(checkboxStatus);
-}) */
-
-/* document.getElementById("submit_2").onclick = function(){
-
-if (document.getElementsByClassName("InputChecks")[0].checked) { 
-    alert("Чекбокс нажат -вариант №3"); 
-} 
-else { alert("Чекбокс не нажат-вариант №3"); }
-} */
